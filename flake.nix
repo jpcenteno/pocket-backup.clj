1ce61463b0e2e4730232f719c0c3818207d8c35d
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2025-07-03 12:08:59 -0300

    chore: Initialize Nix flake for a Clojure project

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Pocket (GetPocket) data exporter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clojure
+            clojure-lsp
+          ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
